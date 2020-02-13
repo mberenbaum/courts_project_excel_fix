29efa3ff4a9642f02622c20059a681fd9655a68d
mberenbaum <michaelberen@gmail.com>
2020-02-13 15:34:45 +0200

    fixed excel

--- read_excel.R
+++ read_excel.R
@@ -2,17 +2,17 @@ library(tidyverse)
 
 library(readxl)
 
-cases_tbl<-read_xlsx("read_excel/iscd_cases_2018-12-05.xlsx" )
+cases_tbl<-read_xlsx("iscd_cases_2018-12-05.xlsx" )
 
 
 cases_tbl_fix<- data.frame(lapply(cases_tbl, function(x) {
   
   gsub(pattern = "," , replacement = " ", x)}))
 
-library(xlsx)
+library(writexl)
 
 write_xlsx(
   cases_tbl_fix, 
-  path = "read_excel/cases_fix.xlsx",
+  path = "cases_fix.xlsx",
   col_names = TRUE
   )
