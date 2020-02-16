f635d5097bbb04f2b9b03ae58c079eb85da991d4
mberenbaum <michaelberen@gmail.com>
2020-02-16 19:24:03 +0200

    fixed the judges file too

--- read_excel.R
+++ read_excel.R
@@ -2,6 +2,10 @@ library(tidyverse)
 
 library(readxl)
 
+library(writexl)
+
+# reading and fixing the cases table
+
 cases_tbl<-read_xlsx("iscd_cases_2018-12-05.xlsx" )
 
 
@@ -9,10 +13,26 @@ cases_tbl_fix<- data.frame(lapply(cases_tbl, function(x) {
   
   gsub(pattern = "," , replacement = " ", x)}))
 
-library(writexl)
 
 write_xlsx(
   cases_tbl_fix, 
   path = "cases_fix.xlsx",
   col_names = TRUE
   )
+
+
+# reading and fixing the judges table
+
+judges_tbl<-read_xlsx("iscd_judges2018-12-05.xlsx" )
+
+
+judges_tbl_fix<- data.frame(lapply(judges_tbl, function(x) {
+  
+  gsub(pattern = "," , replacement = " ", x)}))
+
+
+write_xlsx(
+  judges_tbl_fix, 
+  path = "judges_fix.xlsx",
+  col_names = TRUE
+)
